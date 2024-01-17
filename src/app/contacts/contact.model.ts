d9f1722598ecbdc147b6f836e230989b9d1612ed
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-01-17 14:48:33 -0300

    updates

---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,16 @@
+export class Contact {
+	constructor(public id: string, public name: string, public email: string, public phone: string, public imageUrl: string, public group: string[] = []){}
+}
+
+// export class Recipe {
+// 	public name: string;
+// 	public description: string;
+// 	public imagePath: string;
+
+// 	constructor(name: string, desc: string, imagePath: string) {
+// 		this.name = name;
+// 		this.description = desc;
+// 		this.imagePath = imagePath;
+// 	}
+
+// }
