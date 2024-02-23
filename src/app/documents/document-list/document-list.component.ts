86d308f9c6cd402bd172ecf650aff3c0a8ab9883
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-02-23 14:38:05 -0300

    update

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -22,8 +22,6 @@ export class DocumentListComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.documentService.documentChangedEvent
-    //   .subscribe((documents: Document[]) => {this.documents = documents})
     this.subscription = this.documentService.documentListChangedEvent
       .subscribe((documentList: Document[]) => {
           this.documents = documentList
