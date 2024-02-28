1d7a9c365e92e0a2b29db443d8598f156832e812
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-02-28 10:06:35 -0300

    updates

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cms-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.css'
 })
-export class DocumentEditComponent {
-onCancel() {
+export class DocumentEditComponent implements OnInit {
+onSubmit(_t4: any) {
 throw new Error('Method not implemented.');
 }
+  
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onCancel() {
+    throw new Error('Method not implemented.');
+  }
 
 }
